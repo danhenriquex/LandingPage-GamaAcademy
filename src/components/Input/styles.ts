b34abdeded46ad71cf00420ae19a3bb5fbaf1a2a
danilohenrique <danilosantana@mat.ci.ufpb.br>
2021-07-18 17:52:20 -0300

    Update

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,10 +4,19 @@ import { theme } from "../../global/theme";
 export const InputStyle = styled.input`
   border-radius: 10px;
   height: 50px;
-  width: 80%;
+  width: 100%;
   border: none;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+
+  padding: 10px;
+
   &:focus {
     outline: none;
+  }
+
+  ::placeholder {
+    padding-left: 2px;
   }
 `;
 
@@ -17,36 +26,36 @@ export const InputContainer = styled.span`
   justify-content: center;
   width: 100%;
 
-  input[type="text"] {
-    padding: 10px;
-  }
-
-  input[type="text"]::placeholder {
-    padding-left: 2px;
-  }
-
   .icon {
     color: ${theme.colors.primary};
-    margin-bottom: 5px;
+    /* margin-bottom: 5px;
+    margin-right: 16px; */
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 
   @media only screen and (max-width: 1198px) {
-    width: 50%;
+    width: 80%;
   }
 `;
 
 export const Icons = styled.button`
-  position: absolute;
-  margin-left: 360px;
+  background: ${theme.colors.white};
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+  border: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   margin-top: 25px;
-  background: transparent;
   border-left: 8px;
   border: none;
   cursor: pointer;
   height: 50px;
   width: 50px;
 
-  @media only screen and (max-width: 1198px) {
+  /* @media only screen and (max-width: 1198px) {
     margin-right: 200px;
-  }
+  } */
 `;
